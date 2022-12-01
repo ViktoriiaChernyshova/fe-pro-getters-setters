9d09b2c176f82ae8e9590f6bf53e79d8373f32db
ViktoriiaChernyshova <96884643+ViktoriiaChernyshova@users.noreply.github.com>
2022-12-01 16:32:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,4 +20,28 @@ export const school = {
     6: new Student('Eugene', [97, 34, 78, 85, 98, 65]),
     7: new Student('Ivan', [76, 89, 78, 98, 98, 99, 89, 96]),
   },
+  get aGradeStudents() {
+    return Object.values(this.students)
+      .filter((item) => item.averageGrade >= 90 && item.averageGrade <= 100)
+      .map((item) => item.name)
+      .join(', ');
+  },
+  get bGradeStudents() {
+    return Object.values(this.students)
+      .filter((item) => item.averageGrade >= 75 && item.averageGrade <= 89)
+      .map((item) => item.name)
+      .join(', ');
+  },
+  get cGradeStudents() {
+    return Object.values(this.students)
+      .filter((item) => item.averageGrade >= 60 && item.averageGrade <= 75)
+      .map((item) => item.name)
+      .join(', ');
+  },
+  get dGradeStudents() {
+    return Object.values(this.students)
+      .filter((item) => item.averageGrade >= 0 && item.averageGrade <= 59)
+      .map((item) => item.name)
+      .join(', ');
+  },
 };
